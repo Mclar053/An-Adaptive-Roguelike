f15296d6f707b6d7018f199ac8083e3a5b78c33f
mclar053 <matrclark@gmail.com>
2017-02-28 22:51:24 +0000

    Changed input settings to only allow wasd as movement keys. Added basic player movement.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,7 +16,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	void FixedUpdate () {
 		//Gets the movement vector for the player
-		Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		Vector2 movementVector = new Vector2(Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw("Vertical"));
 		//Adds a force to the player for the direction they are going
 		//The movementvector does not exceed 1 meaning that diagonals are just as fast as moving horizontally or vertically
 		GetComponent<Rigidbody2D>().AddForce (Vector3.ClampMagnitude(movementVector,1) * playerSpeed);
